6cff31fd9a558f4047acefc523e50fbc1491c720
DESKTOP-Q4EHISO\Toni <anton_krizmanic@hotmail.com>
2017-01-29 12:26:19 +0100

    For heroku

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
-$database = substr($url["path"], 1);*/
+$database = substr($url["path"], 1);
 return [
 
     /*
@@ -71,7 +71,7 @@ return [
             'engine' => null,
         ],
         //heroku
-        /*'mysql' => [
+        'mysql' => [
             'driver'    => 'mysql',
             'host'      => $host,
             'database'  => $database,
@@ -80,7 +80,7 @@ return [
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-        ],*/
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
